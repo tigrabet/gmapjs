cac47f6ed27c77b7293d850949e8f605eecf5f33
a <b>
2017-08-17 11:13:29 +0200

    test

--- customizemap.js
+++ customizemap.js
@@ -1,7 +1,7 @@
-if (typeof gmapjs === 'undefined') {
-  gmapjs = {}
-}
 gmapjs.customizeMap = function(map) {
+  if (typeof gmapjs === 'undefined') {
+    gmapjs = {}
+  }
   var markerIcon = {
     url: 'https://2.bp.blogspot.com/-TmObZ6JmECw/WY1VSvJdx7I/AAAAAAAAAF4/yZLSuWJB-jc8s6BjU009yvpTVEbcHv43ACLcBGAs/s1600/oak.png',
     labelOrigin: new google.maps.Point(11, 50)
